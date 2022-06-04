2f39fd16c3e1d097f4d9617e3eba245a251d34fc
Melifo <106874021+Melifo@users.noreply.github.com>
2022-06-04 17:10:09 +0300

    Update 03_task_if_else.py

--- Module2/practice/if_else/03_task_if_else.py
+++ Module2/practice/if_else/03_task_if_else.py
@@ -3,3 +3,11 @@
 # Формат выходных данных: Выведите «YES», если треугольник равнобедренный, и «NO» в противном случае.
 
 # TODO: your code here
+side1_triangle= float(input("Сторона 1:"))
+side2_triangle= float(input("Сторона 2:"))
+side3_triangle= float(input("Сторона 3:"))
+
+if side1_triangle==side2_triangle or side1_triangle==side3_triangle or side2_triangle==side3_triangle:
+    print("Yes")
+else:
+    print("No")
