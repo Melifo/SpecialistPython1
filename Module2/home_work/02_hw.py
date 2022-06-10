38da26e443c032840726ec43e3efa181c45cd0e8
Melifo <106874021+Melifo@users.noreply.github.com>
2022-06-10 22:26:14 +0300

    Update 02_hw.py
    Исправление. Заметила ошибку при склонении 111-114 , 1213 и т.д.
    Не нашла как исправить в предыдущем сообщении. Отправляю снова.

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -7,4 +7,13 @@
 # коров, корова или коровы
 # Например, 1 корова, 2 коровы, 5 коров, 125 коров.
 
-# TODO: your code here
+cow_number = int(input("Сколько коров склонять будем?:"))
+
+if cow_number%100 == 11 or cow_number%100 == 12 or cow_number%100 == 13 or cow_number%100 == 14 :
+    print(cow_number, "krorov")
+elif cow_number % 10 == 1:
+    print(cow_number, "krorova")
+elif cow_number % 10 == 2 or cow_number % 10 == 3 or cow_number % 10 == 4:
+    print(cow_number, "krorovy")
+else:
+    print(cow_number, "krorov")
