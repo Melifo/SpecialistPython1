237b037b5cb2cfa2021c43631f0d4470b6c975c0
Melifo <106874021+Melifo@users.noreply.github.com>
2022-06-19 21:58:42 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,11 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+
+max_len = len(names[0])
+name_max = 0
+for name in names:
+    if len(name) > max_len:
+        max_len = len(name)
+        name_max = name
+print(f"{name_max} - {max_len} букв")
