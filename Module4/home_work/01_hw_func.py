73a564beb02af36c5fb03d7aa1821643a9a845b1
Diana <106874021+Melifo@users.noreply.github.com>
2022-06-23 22:23:05 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -3,7 +3,24 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
+
+    ticket = str(ticket_number)
+    number_quantity = len(ticket)
+
+    if number_quantity % 2 == 0:
+        mid = int(number_quantity / 2)
+        lucky_test = [int(number) for number in ticket]
+        if sum(lucky_test[:mid]) == sum(lucky_test[mid:]):
+            return True
+        else:
+            return False
+    else:
+        mid = int(number_quantity / 2) + 1
+        lucky_test = [int(number) for number in ticket]
+        if sum(lucky_test[:mid - 1]) == sum(lucky_test[mid:]):
+            return True
+        else:
+            return False
     pass
 
 
