3b5c6c2a62b650003216f2c08642bd532b4eb540
Diana <106874021+Melifo@users.noreply.github.com>
2022-06-30 21:38:54 +0300

    Update [hw]03_task_string.py

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,15 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = input("Введите текст:")
+consonants = "бвгдзклмнпрстфхцчщ"
+text_low = text.lower()
+
+total_consonants = 0
+i = 0  # индекс нужной буквы
+
+for _ in consonants:
+    counting = text_low.count(consonants[i])
+    total_consonants += counting
+    i += 1
+
+print(f"{total_consonants} согласных букв в тексте")
